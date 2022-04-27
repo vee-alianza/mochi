eaf8359e5841bb4ad27bbe251c9cb7f994f3a089
vee-alianza <vee.alianza@gmail.com>
2022-04-26 22:39:53 -0600

    completed login validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
 // Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -36,6 +49,20 @@ router.delete(
     (_req, res) => {
         res.clearCookie('token');
         return res.json({ message: 'success' });
+    }
+);
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
     }
 );
 
