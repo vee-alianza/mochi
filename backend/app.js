20b6f16b7fd6a4daede1b54fb50d7e8966bdf96f
vee-alianza <vee.alianza@gmail.com>
2022-04-26 21:24:48 -0600

    completed phase 2 error handling

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const { environment } = require('./config');
 const isProduction = environment === 'production';
 const app = express();
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -39,5 +40,37 @@ app.use(
 );
 
 app.use(routes); // Connect all the routes
+
+//ERROR HANDLING
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
