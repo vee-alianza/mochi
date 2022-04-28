d9c47641a033c3b66ddf1a3bd7e374670e9e13eb
vee-alianza <vee.alianza@gmail.com>
2022-04-27 21:22:48 -0600

    login form session actions and reducer complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import App from './App';
 import configureStore from './store';
 
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -19,6 +20,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
