e2ad177316282f389f6b99aa06fa3a53de26b715
vee-alianza <vee.alianza@gmail.com>
2022-05-02 11:53:11 -0600

    add storyReducer and import to index.js in store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
+import stories from './stories';
 
 
 const rootReducer = combineReducers({
     session: sessionReducer,
+    stories
 });
 
 let enhancer;
---
+++ frontend/src/store/stories.js
@@ -0,0 +1,60 @@
+import { csrfFetch } from './csrf';
+
+const SET_STORIES = "stories/get";
+const ADD_STORY = "stories/add";
+
+// Actions
+// export to use globally to components (to trigger actions)
+export const setStories = (stories) => {
+  return {
+    type: SET_STORIES,
+    payload: stories
+  }
+}
+
+export const addStory = (story) => {
+  return {
+    type: ADD_STORY,
+    payload: story
+  }
+}
+
+// Thunks (Async Actions)
+// Thunk middleware = dispatch
+export const getStories = () => async dispatch => {
+  const response = await csrfFetch('/api/stories');
+  if (!response.ok) {
+    throw response;
+  }
+  const stories = await response.json();
+  dispatch(setStories(stories));
+};
+
+const initialState = {};
+
+// Reducer
+// normalizing state happens in Reducer
+const storyReducer = (stories = initialState, action) => {
+  switch (action.type) {
+    case SET_STORIES:
+      const stories = action.payload;
+      // return stories.reduce((newStories, story) => {
+      //   return {
+      //     ...newStories,
+      //     [story.id]: story
+      //   }
+      // }, {});
+
+      const newStories = {};
+      for (const story of stories) {
+        newStories[story.id] = story;
+      }
+      return newStories;
+    case ADD_STORY:
+      return
+    default:
+      return stories;
+  }
+};
+
+export default storyReducer;
