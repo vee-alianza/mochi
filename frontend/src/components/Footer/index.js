44781b52adc6843361c168e65bc82ff9e166203c
vee-alianza <vee.alianza@gmail.com>
2022-05-09 06:09:08 -0600

    add footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import "./footer.css";
+import "./Footer.css";
 
 const Footer = () => {
     return (
         <footer>
-            <span>
-                <a href="https://github.com/vee-alianza">
-                    <i className="fa-brands fa-github-alt"></i>
-                </a>
-            </span>
+            <div className="footer__info">
+                <div className="contact__info">
+                    <span>
+                        <a href="https://github.com/vee-alianza">
+                            <img src="https://user-images.githubusercontent.com/92604480/167403399-04f7b726-4f98-4f60-bd99-c85cec3b8ba3.png" />
+                        </a>
+                    </span>
+                </div>
+            </div>
         </footer>
     )
 
 }
 
 export default Footer;
-
-// https://user-images.githubusercontent.com/92604480/167149343-1d74c9fc-9374-4824-a6cb-c780a03e6a13.png
